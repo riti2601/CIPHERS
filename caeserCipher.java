3f72fae2042885d31aa6a0187f755a6ed976179d
riti2601 <ritishapande@gmail.com>
2024-08-21 22:15:35 +0530

    caeser cipher - encryption and decryption

--- caeserCipher.java
+++ caeserCipher.java
@@ -7,18 +7,44 @@ public class caeserCipher{
         String name = read.nextLine();
         int age = read.nextInt();
         read.nextLine(); // this is added so the the input is handled better, as without it the if condition was being ignored
+
         if(age < 18){
             System.out.println("I'm sorry you can not encrypt the data. You are very young to hide anything!");
             return;
         }
-        System.out.println("Welcome "+name+"!\nPlease enter the data that is to be encrypted, we will be using Caeser Cipher to encrypt the data.");
-        String data = read.nextLine();
-        System.out.println("Choose any number between 1 to 9:");
-        int k = read.nextInt();
+        
+        System.out.println("Welcomev"+name+"!\nDo you want to encrypt data or decrypt the data?\nFor encryption enter: 1\nFor decryption enter: 2");
+        int x = read.nextInt();
         read.nextLine();
+
+        switch (x) {
+            case 1:
+                System.out.println("\nPlease enter the data that is to be encrypted, we will be using Caeser Cipher to encrypt the data.");
+                String data = read.nextLine();
+                read.nextLine();
+                System.out.println("Choose any number between 1 to 9:");
+                int k = read.nextInt();
+                
+                String encryptedData = encrypt(data,k);
+                System.out.println("Encrypted data: " + encryptedData);
+                break;
+            
+            case 2:
+                System.out.println("\nPlease enter the data that is to be decrypted, we will be using Caeser Cipher to decrypt the data.");
+                data = read.nextLine();
+                read.nextLine();
+                System.out.println("Enter the number between 1 to 9 which was used for encryption before:");
+                k = read.nextInt();
+                
+                String decryptedData = decrypt(data,k);
+                System.out.println("Decrypted data: " + decryptedData);
+                break;
+        
+            default:
+                System.out.println("The input was invalid. Kindly restart.");
+                break;
+        }
     
-        String encryptedData = encrypt(data,k);
-        System.out.println("Encrypted data: " + encryptedData);
     }
 
     public static String encrypt(String data, int k){
@@ -30,6 +56,22 @@ public class caeserCipher{
             }
             else{
                 ch[i] = (char) (ch[i] + k);
+            }
+        }
+        
+        return new String(ch);
+    }
+
+
+    public static String decrypt(String data, int k){
+        char[] ch = new char[data.length()];
+        for(int i=0; i< data.length(); i++){
+            ch[i] = data.charAt(i);
+            if(ch[i] == '*'){
+                ch[i] = ' ';
+            }
+            else{
+                ch[i] = (char) (ch[i] - k);
             }
         }
         
